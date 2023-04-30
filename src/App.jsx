9480e4f692af1b2e6d6469cbb91ad61c66ed58ba
Kekcik <i@batomunkuevv.ru>
2023-04-30 14:10:48 +0900

    Edits 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,22 @@
-import React from "react";
-import PropTypes from 'prop-types';
-import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import AppHeader from "./components/app-header/app-header";
 import BurgerConstructor from "./components/burger-constructor/burger-constructor";
 import BurgerIngredients from "./components/burger-ingredients/burger-ingredients";
 
-class App extends React.Component {
-    render() {
-        return (
-            <>
-                <AppHeader />
-                <main className="page">
-                    <div className="container">
-                        <div className="page__body pl-5 pr-5">
-                            <BurgerIngredients />
-                            <BurgerConstructor />
-                        </div>
+const App = () => {
+    return (
+        <>
+            <AppHeader />
+            <main className="page">
+                <div className="container">
+                    <div className="page__body pl-5 pr-5">
+                        <BurgerIngredients />
+                        <BurgerConstructor />
                     </div>
-                </main>
-            </>
-        );
-    }
+                </div>
+            </main>
+        </>
+    );
+
 }
 
 export default App;
